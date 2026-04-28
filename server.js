705e67e778a0279e600a64fba379684061f3258c
Kanishka Tyagi <krishrk108@gmail.com>
2026-04-28 15:43:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors({
-  origin: "https://weather-analytics-frontend-gzh5q70x7-sachiikrishs-projects.vercel.app",
+  origin: true,
+  credentials: true
 }));
 app.use(express.json());
 
